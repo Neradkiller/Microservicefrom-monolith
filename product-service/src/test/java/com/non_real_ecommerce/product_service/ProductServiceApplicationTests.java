d619077ffc34424497a32bb1a0e6ab1f92798d0d
Neradkiller <daren1997gonzalez@gmail.com>
2025-11-12 20:24:18 -0400

    Initial commit: estructura basica de los servicios

---
+++ product-service/src/test/java/com/non_real_ecommerce/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.non_real_ecommerce.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/com/non_real_ecommerce/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.non_real_ecommerce.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
